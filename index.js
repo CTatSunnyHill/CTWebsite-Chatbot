c5681b56c69f01fe3a561e4dacc8f19d8450a729
mursalinreza <mursalinreza2902@gmail.com>
2024-02-27 10:02:25 -0800

    Server side setup commit

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const app = express()
 
 const port = process.env.PORT || 3001
 
+app.get('',(req,res)=>{
+    res.send("Server side app set up initiated")
+})
+
 app.listen(port, ()=>{
     console.log(chalk.green.bold("Server side app is running on port 3001"))
 })
