99edcca8444e05ece6a49e8a4efa83341a351934
mursalinreza <mursalinreza2902@gmail.com>
2024-02-28 10:46:14 -0800

    Test fix

--- router/dialogFlowRouter.js
+++ router/dialogFlowRouter.js
@@ -2,6 +2,14 @@ const express = require('express')
 const chatbot = require('../chatbot/chatbot')
 const router = new express.Router()
 
+router.get('/', (req,res)=>{
+  try{
+    res.status(201).send("Welcome to Sunny Hill ChatBot")
+  }catch(e){
+    res.status(400).send()
+  }
+})
+
 router.post('/api/df_text_query', async (req,res)=>{
   try{
     const response = await chatbot.textQuery(req.body.text,req.body.parameters)
