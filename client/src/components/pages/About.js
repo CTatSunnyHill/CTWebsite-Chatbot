23dc3882ec7490305fe8fe9fa8e8d8237b302400
mursalinreza <mursalinreza2902@gmail.com>
2024-04-22 10:27:37 -0700

    Final Commit from Backend

--- client/src/components/pages/About.js
+++ client/src/components/pages/About.js
@@ -4,10 +4,13 @@ const About = () => (
         <div>
             <h5> Director - The Motion Lab and CLinical Technology</h5>
             <p> Dr.Tim Bhatnagar</p>
+            <p> Contact - 1234567890</p>
             <h5> Clinical Technology Assistant</h5>
             <p> Mukhinder Gill</p>
+            <p> Contact - 1234567890</p>
             <h5> Clinical Technology Support Co-op Student</h5>
             <p> Reza Mursalin Sabid</p>
+            <p> Contact - 1234567890</p>
 
         </div>
     )
